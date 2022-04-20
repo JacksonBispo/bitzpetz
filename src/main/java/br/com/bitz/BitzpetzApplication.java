5b2776f4428bea6ab5448285f69cdd03c0267661
Jackson Novaes <jackson.novaes@live.com>
2022-04-19 23:21:00 -0300

    initial commit

---
+++ src/main/java/br/com/bitz/BitzpetzApplication.java
@@ -0,0 +1,13 @@
+package br.com.bitz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitzpetzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitzpetzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bitz/BitzpetzApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bitz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitzpetzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
