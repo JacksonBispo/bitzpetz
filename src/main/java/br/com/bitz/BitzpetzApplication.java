21e0bab521f857714f71e2e1311c46e9e6c89d24
Jackson Novaes <jackson.novaes@live.com>
2022-04-19 23:39:08 -0300

    merge

--- src/main/java/br/com/bitz/BitzpetzApplication.java
+++ src/main/java/br/com/bitz/BitzpetzApplication.java
@@ -3,7 +3,7 @@ package br.com.bitz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class BitzpetzApplication {
 
 	public static void main(String[] args) {
